e8fefcb0108fe14d65554bf3dbf44bba42cff046
tipsypastels <dakotasankey@gmail.com>
2019-09-06 20:48:31 -0700

    meh

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import MouseCircle from './MouseCircle';
 
 import Page from './Pages/Page';
@@ -9,18 +10,35 @@ import Bots from './Pages/Bots';
 
 import './App.scss';
 
+const supportsHistory = 'pushState' in window.history;
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <MouseCircle />
       
-      <Router>
+      <Router forceRefresh={!supportsHistory}>
         <Page>
-          <Switch>
-            <Route path="/contact" exact component={Contact} />
-            <Route path="/bots" exact component={Bots} />
-            <Route path="/" exact component={Hello} />
-          </Switch>
+          <Route render={({ location }) => {
+            const { pathname } = window.location;
+            return (  
+              <TransitionGroup>
+                <CSSTransition
+                  key={pathname}
+                  classNames="Page__transition"
+                  timeout={{ enter: 1000, exit: 1000 }}
+                >
+                  <Route location={location} render={() => (
+                    <Switch>
+                      <Route path="/contact" exact component={Contact} />
+                      <Route path="/bots" exact component={Bots} />
+                      <Route path="/" exact component={Hello} />
+                    </Switch>
+                  )} />
+                </CSSTransition>
+              </TransitionGroup>
+            );
+          }} />
         </Page>
       </Router>
     </div>
