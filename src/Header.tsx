922883e0574fa1a2f23c7935a5a01b7ca06c0162
tipsypastels <dakotasankey@gmail.com>
2019-09-08 20:48:32 -0700

    header image

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 
+import me from './images/me.png';
 import './Header.scss';
 
 export default function Header() {
   return (
     <div className="Header">
-      <div className="Header__logo">
+      <div className="Header__logo" aria-hidden="true">
         <span>d</span>
         <span>k</span>
         <span>t</span>
       </div>
 
+      <img 
+        src={me} 
+        className="Header__img"
+        alt=""
+      />
+
       <div className="Header__bar">
         <div className="Header__name">
           Dakota Sankey
         </div>
-
-        <div className="Header__spacer" />
 
         <nav className="Header__nav">
           <a href="https://github.com/tipsypastels">
